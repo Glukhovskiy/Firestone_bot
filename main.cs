f16990cc65e487b32735db8995f375239cde55c6
Glukhovskiy <dima@glukhovskiy.by>
2025-07-26 21:18:11 +0300

    Bump version to 0.14.0

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text.Json;
 
-[assembly: MelonInfo(typeof(Firestone_bot.BotMain), "Firestone Bot", "0.13.2", "GentleDevil")]
+[assembly: MelonInfo(typeof(Firestone_bot.BotMain), "Firestone Bot", "0.14.0", "GentleDevil")]
 [assembly: MelonGame("HolydayStudios", "Firestone")]
 
 namespace Firestone_bot
